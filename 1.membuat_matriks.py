0653d8cd00f9de83f2c10ae39ef6b340d84aa96d
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-04-15 08:30:33 +0700

    Update 1.membuat_matriks.py

--- 1.membuat_matriks.py
+++ 1.membuat_matriks.py
@@ -4,3 +4,9 @@ matriks = np.array([[1,2,3],[4,5,6]])
 
 print("matriks: ")
 print(matriks)
+
+# matriks kompleks
+matriks_kompleks = np.array([[2+1j,3-2j],[3+4j,3-1j]])
+
+print("matriks kompleks :")
+print(matriks_kompleks)
