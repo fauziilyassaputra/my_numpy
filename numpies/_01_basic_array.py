1ceed3de4f929b614abb6696167746ef20d89b32
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-21 09:32:48 +0700

    Create _01_basic_array.py

---
+++ numpies/_01_basic_array.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# array with zero
+a = np.zeros(3)
+# output : [0. 0. 0.]
+
+
+# array with 1
+b = np.ones(2)
+# output : [1. 1.]
+
+
+# array dengan range
+c = np.arange(6)
+# output : [0 1 2 3 4 5]
+
+
+#  array dengan range ( angka pertama, angka terakhir, step ukuran )
+d = np.arange(1,20,2)
+print(d)
+# output : [ 1  3  5  7  9 11 13 15 17 19]
+e = np.arange(5,30,5)
+print(e)
+# output : [ 5 10 15 20 25]
+
