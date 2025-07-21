1ee243da3433e8dc63b2c30df22ef1dfca927558
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-21 20:35:04 +0700

    Create _1D_arraycreation.py

---
+++ numpies/array_creation/_1D_arraycreation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print(np.arange(10))
+# output : [0 1 2 3 4 5 6 7 8 9]
+
+print(np.arange(2, 10, dtype=float))
+# output : [2. 3. 4. 5. 6. 7. 8. 9.]
+
+print(np.arange(2, 3, 0.1))
+# output : [2.  2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9]
+
+# praktik terbaik dalam menggunakan .range takkala menggunakan awal ,akhir dan step tertentu nilai integer.
