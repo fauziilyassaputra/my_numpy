3183bb0da9bad11d75be3d65e308df12f313e440
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-29 06:07:27 +0700

    Update pandas_2.py

--- pandas/pandas_2.py
+++ pandas/pandas_2.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-file = pd.read_csv('datacsv.csv')
+file = pd.read_csv('datacsv.csv') # total ada 891 data
 
-pd.options.display.min_rows = 891
+pd.options.display.min_rows = 891 # agar menghasilkan output seluruhnya tanpa dibatasi
 pd.options.display.max_rows = 891
 
-# print(file.info())
-# print(file.describe()
+# print(file.info())  # memberikan ringkasan tentang kolom, tipe data , jumlah non-null, dan penggunaan memori
+# print(file.describe() # menyediakan statistik deskriptif untuk kolom numerik secara default
 print(file.describe(include= "O"))
 
+
+
+# reference :
+# options = https://pandas.pydata.org/docs/user_guide/options.html
 
