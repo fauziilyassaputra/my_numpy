3630c71ac9245e124e2291d22f96fe3b3186dc5a
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-29 05:54:19 +0700

    Update pandas_1.py

--- pandas/pandas_1.py
+++ pandas/pandas_1.py
@@ -9,15 +9,18 @@ baca_awal_file = file.head()
 baca_akhir_file = file.tail()
 # print(baca_akhir_file)
 
-
-file.to_csv("datacsv.csv", index=False)
+# membuat file csv baru yang berisi data csv lama
+file.to_csv("datacsv.csv", index=False) # index false agar index dataframe tidak ikut disimpan di csv
 file_baru = pd.read_csv('datacsv.csv')
 # print(file_baru)
 
+# mengubah ke excel
 file_baru.to_excel('dataexcel.xlsx', index=False, sheet_name='asik')
 file_excel = pd.read_excel('dataexcel.xlsx')
 # print(file_excel)
 
+
+# mengubah ke html
 file_html = pd.read_html('https://www.fdic.gov/bank/individual/failed/banklist.html')
 
 type(file_html)
@@ -25,6 +28,7 @@ cek_data_html = file_html[0]
 # print(cek_data_html)
 
 
+# mengubah ke data sql
 mesin = create_engine('sqlite:///:memory:')
 file_baru.to_sql('datasql', con=mesin)
 baca_sql = pd.read_sql('datasql', con=mesin)
